79d328593dff00992a3541e0bc663bec3ee8088d
Tank 228 <92095847+tankoman228@users.noreply.github.com>
2023-04-01 23:29:11 +0700

    SQL warning added

--- NewReport.sql
+++ NewReport.sql
@@ -1,3 +1,4 @@
 INSERT INTO Reports (Header, Date, Label, Link_Document)
 VALUES ('ERROR', GETDATE(), 'E', 'https://natk.ru/stud-grad/schedule/187?group=%D0%9F%D0%A0-21.102')
 /*Inserts new report into DB*/
+/*Внимание! Смотрите документацию, не вставляйте отсебятину в Label!!!!!*/
